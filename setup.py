b9d7ec7e4cc4443b35070b5cd6e024caaa575b85
mikedatsik <mikedatsik@gmail.com>
2017-01-23 12:31:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import os
+
 from setuptools import setup
+
+ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+SOURCE_PATH = os.path.join(ROOT_PATH, 'source')
+README_PATH = os.path.join(ROOT_PATH, 'README.rst')
 
 setup(
     name='pw_MultiScriptEditor',
@@ -7,6 +13,7 @@ setup(
     url='https://github.com/mikedatsik/pw_MultiScriptEditor',
     author='paulwinex',
     license='Apache License (2.0)',
+    packages=find_packages(SOURCE_PATH),
     package_dir={
         '': 'source'
     },
