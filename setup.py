fadf27b9c5b49d11244194b24beaf8f8bd13a6d5
mikedatsik <mikedatsik@gmail.com>
2017-01-23 11:52:22 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pw_MultiScriptEditor',
+    version='2.0.3',
+    description='Python Editor for CG Applications',
+    url='https://bitbucket.org/ftrack/QtExt',
+    author='paulwinex',
+    license='Apache License (2.0)'
+)
